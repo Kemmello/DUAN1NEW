141d261b37dd4282ea41c9e60fea3bf9fcee96eb
Kemmello <kemmello2002@gmail.com>
2021-11-10 22:12:26 +0700

    update thử

--- app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
@@ -30,15 +30,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//                ImageSlider imageSlider = findViewById(R.id.slider);
-//        List<SlideModel> slideModels = new ArrayList<>();
-//        slideModels.add(new SlideModel("https://www.pixelstalk.net/wp-content/uploads/2016/11/Dandelion-Background-Full-HD.jpg","1 Image"));
-//        slideModels.add(new SlideModel("https://scr.vn/wp-content/uploads/2020/07/H%C3%ACnh-n%E1%BB%81n-Anime-4k-cho-desktop.jpg","2 Image"));
-//        slideModels.add(new SlideModel("http://3.bp.blogspot.com/-Cb0sId5ZlhY/U4RDKxBRFxI/AAAAAAAARvk/-5tjDo7U9FE/s1600/Winter-2.jpg","3 Image"));
-//        slideModels.add(new SlideModel("http://1.bp.blogspot.com/-9B2JNcvWR_E/VFbtDZuaCCI/AAAAAAAAG_U/roxyNysD_RI/s1600/phong-canh-3d-13.jpg","4 Image"));
-//        slideModels.add(new SlideModel("https://socbay.mobi/publics/uploads/2-2020/post/13053449290220_bo-hinh-nen-desktop-do-phan-giai-cao.jpg","5 Image"));
-//        imageSlider.setImageList(slideModels,true);
-
         tvShowMoreExplore = view.findViewById(R.id.tvShowMoreExplore);
         tvShowMoreNew = view.findViewById(R.id.tvShowMoreNew);
         tvShowMoreSell = view.findViewById(R.id.tvShowMoreSell);
