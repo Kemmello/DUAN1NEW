339d060e5a76e9a2585ebbedfefc05dbe06fa18f
nhokxinh8778 <lupham011102@gmail.com>
2021-11-11 00:02:55 +0700

    Revert "Merge pull request #5 from Kemmello/BinhBranch"
    This reverts commit 6a2099a613badd2959abc03c62153e093510434c, reversing
    changes made to f35509d15ca17f0c291b589068655b57a6b2aba1.

--- app/src/main/java/com/example/myapplication/fragment/ExitFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/ExitFragment.java
@@ -3,22 +3,25 @@ package com.example.myapplication.fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.myapplication.R;
+import com.example.myapplication.activities.MainActivity;
 import com.example.myapplication.activities.SignUpActivity;
+
 
 public class ExitFragment extends Fragment {
 
-    Button btnLogOut;
+Button btnLogOut;
 
     public ExitFragment() {
         // Required empty public constructor
