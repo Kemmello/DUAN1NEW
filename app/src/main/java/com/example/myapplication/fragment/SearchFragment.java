45553ac798eff6b5da7b7f3e309226227fcae5ed
dangphps18367 <hin200000@gmail.com>
2021-11-19 22:41:12 +0700

    update search

---
+++ app/src/main/java/com/example/myapplication/fragment/SearchFragment.java
@@ -0,0 +1,137 @@
+package com.example.myapplication.fragment;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.adapter.BookAllAdapter;
+import com.example.myapplication.model.Book;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchFragment extends Fragment {
+    EditText search_box;
+    ImageView ivSearch;
+
+    private List<Book> viewAllBookList;
+    private RecyclerView recyclerViewSearch;
+    private BookAllAdapter bookAllAdapter;
+    //List
+    FirebaseFirestore firestore;
+
+
+    //Slide
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+        firestore = FirebaseFirestore.getInstance();
+        recyclerViewSearch = root.findViewById(R.id.search_rec);
+        search_box = root.findViewById(R.id.edtSearchBox);
+        ivSearch = root.findViewById(R.id.exitSearch);
+        viewAllBookList = new ArrayList<>();
+        bookAllAdapter = new BookAllAdapter(getContext(), viewAllBookList);
+        recyclerViewSearch.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        recyclerViewSearch.setAdapter(bookAllAdapter);
+        recyclerViewSearch.setHasFixedSize(true);
+        search_box.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String editable = search_box.getText().toString();
+                if (editable.toString().isEmpty()) {
+                    viewAllBookList.clear();
+                    bookAllAdapter.notifyDataSetChanged();
+                } else {
+                    searchProduct(editable.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new Fragment();
+                fragment = new HomeFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame,fragment).commit();
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+    private void searchProduct(String book) {
+        if (!book.isEmpty()) {
+//            firestore.collection("BOOK").whereEqualTo("TITLE",book).get()
+//                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                            if (task.isSuccessful() && task.getResult() != null){
+//                                viewAllBookList.clear();
+//                                bookAllAdapter.notifyDataSetChanged();
+//                                for (DocumentSnapshot doc: task.getResult().getDocuments()){
+//                                    Book viewAllBook = doc.toObject(Book.class);
+//                                    viewAllBookList.add(viewAllBook);
+//                                    bookAllAdapter.notifyDataSetChanged();
+//                                }
+//                            }
+//                        }
+//                    });
+            firestore.collection("BOOK").orderBy("TITLE").get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                String title = document.getString("TITLE");
+                                if (title.contains(book)) {
+                                    Book bookfound = document.toObject(Book.class);
+                                    viewAllBookList.add(bookfound);
+                                    bookAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+
+        }
+    }
+}
